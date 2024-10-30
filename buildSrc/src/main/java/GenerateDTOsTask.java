b0d375b6e7db0776dde93c9062eb35de2605de2e
Florian Stascheck <fstascheck@tripadvisor.com>
2024-10-30 14:27:27 +0000

    DTO code generator, now with @Nonnull annotations

--- buildSrc/src/main/java/GenerateDTOsTask.java
+++ buildSrc/src/main/java/GenerateDTOsTask.java
@@ -2,7 +2,6 @@ package io.bokun.octo.gradle;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Predicate;
 
 import org.yaml.snakeyaml.Yaml;
 
@@ -26,6 +26,34 @@ public class GenerateDTOsTask {
     public static void generateDTOs(String filename, String buildDir) {
         var instance = new GenerateDTOsTask(filename, buildDir);
         instance.generate();
+    }
+
+    private static class Type {
+        public String type;
+        public Boolean nullable = false;
+        public Boolean required = false;
+
+        public Type(String type) {
+            this.type = type;
+        }
+
+        public Type nullable(boolean nullable) {
+            this.nullable = nullable;
+            return this;
+        }
+
+        public Type required(boolean required) {
+            this.required = required;
+            return this;
+        }
+
+        public Type wrap(String wrapClass) {
+            return new Type(wrapClass + "<" + type + ">").nullable(nullable).required(required);
+        }
+
+        public String toString() {
+            return (nullable ? "" : "@Nonnull ") + type;
+        }
     }
 
     private void registerEnum(String name, ArrayList<String> options) {
@@ -60,16 +88,17 @@ public class GenerateDTOsTask {
         return input;
     }
 
-    private String getType(
+    private Type getType(
             String type,
             Map<String, Object> prop,
             String propName,
             String schemaName,
-            Boolean typeAsSingular
+            Boolean typeAsSingular,
+            Predicate<String> isRequired
     ) {
         if (type.equals("array")) {
             var items = (Map<String, Object>) prop.get("items");
-            return "ArrayList<" + processAndGetType(items, propName, schemaName, true) + ">";
+            return processAndGetType(items, propName, schemaName, true, isRequired).wrap("ArrayList");
         }
 
         if (type.equals("object") && propName.equals("restrictions")) {
@@ -77,71 +106,98 @@ public class GenerateDTOsTask {
         }
 
         return switch (type) {
-            case "string" -> "String";
-            case "boolean" -> "Boolean";
-            case "integer" -> "Integer";
-            case "object" -> makeObjectName(propName, typeAsSingular);
+            case "string" -> new Type("String");
+            case "boolean" -> new Type("Boolean");
+            case "integer" -> new Type("Integer");
+            case "object" -> new Type(makeObjectName(propName, typeAsSingular));
             default ->
                     throw new RuntimeException("getType: " + propName + ": Unknown type: " + type + " " + prop.toString());
         };
     }
 
-    private String processAndGetType(
+    private Type processAndGetType(
             Map<String, Object> item,
             String itemName,
             String schemaName,
-            Boolean typeAsSingular
+            Boolean typeAsSingular,
+            Predicate<String> isRequired
     ) {
         if (item.containsKey("oneOf")) {
-            for(var child : (ArrayList<Map<String, Object>>) item.get("oneOf")) {
-
-                if (! child.containsKey("type")) {
-                    continue;
-                }
-
-                if (child.get("type").equals("null")) {
-                    continue;
-                }
-
-                return processAndGetType(child, itemName, schemaName, typeAsSingular);
-            }
+            return processAndGetTypeOneOf(item, itemName, schemaName, typeAsSingular, isRequired);
         }
 
         var type = item.get("type");
 
         if (type instanceof String) {
-            return processAndGetType((String) type, item, itemName, schemaName, typeAsSingular);
+            return processAndGetType((String) type, item, itemName, schemaName, typeAsSingular, isRequired);
         } else if (type instanceof ArrayList<?>) {
-            return processAndGetType((ArrayList<?>) type, item, itemName, schemaName, typeAsSingular);
+            return processAndGetType((ArrayList<?>) type, item, itemName, schemaName, typeAsSingular, isRequired);
         }
 
         throw new RuntimeException("processAndGetType: " + itemName + ": Unknown type: " + (type != null ? type.getClass() : "null") + " " + item);
     }
 
-    private String processAndGetType(
+    private Type processAndGetTypeOneOf(Map<String, Object> item, String itemName, String schemaName, Boolean typeAsSingular, Predicate<String> isRequired) {
+        Boolean nullable = false;
+        Map<String, Object> foundChild = null;
+
+        for (var child : (ArrayList<Map<String, Object>>) item.get("oneOf")) {
+            if (!child.containsKey("type")) {
+                continue;
+            }
+
+            if (child.get("type").equals("null")) {
+                nullable = true;
+                continue;
+            }
+
+            foundChild = child;
+        }
+
+        if (foundChild == null) {
+            throw new RuntimeException("processAndGetType: " + itemName + ": Unparseable oneOf: no child found");
+        }
+
+        return processAndGetType(foundChild, itemName, schemaName, typeAsSingular, isRequired).nullable(nullable);
+    }
+
+    private Type processAndGetType(
             ArrayList<?> type,
             Map<String, Object> item,
             String itemName,
             String schemaName,
-            Boolean typeAsSingular
+            Boolean typeAsSingular,
+            Predicate<String> isRequired
     ) {
+        String foundType = null;
+        Boolean nullable = false;
+
         for (var t : type) {
-            if (t.equals("null"))
+            if (t.equals("null")) {
+                nullable = true;
                 continue;
+            }
+
             if (!(t instanceof String))
                 continue;
-            return processAndGetType((String) t, item, itemName, schemaName, typeAsSingular);
+
+            foundType = (String) t;
         }
 
-        return "Null";
+        if (foundType == null) {
+            throw new RuntimeException("processAndGetType: Can't find suitable type for " + itemName);
+        }
+
+        return processAndGetType(foundType, item, itemName, schemaName, typeAsSingular, isRequired).nullable(nullable);
     }
 
-    private String processAndGetType(
+    private Type processAndGetType(
             String type,
             Map<String, Object> item,
             String itemName,
             String schemaName,
-            Boolean typeAsSingular
+            Boolean typeAsSingular,
+            Predicate<String> isRequired
     ) {
         if (type.equals("string")) {
             registerEnum((String) item.get("title"), (ArrayList<String>) item.get("enum"));
@@ -151,13 +207,13 @@ public class GenerateDTOsTask {
             processObject(item, makeObjectName(itemName, typeAsSingular));
         }
 
-        return getType(type, item, makeObjectName(itemName, typeAsSingular), schemaName, typeAsSingular);
+        return getType(type, item, makeObjectName(itemName, typeAsSingular), schemaName, typeAsSingular, isRequired);
     }
 
     private void processObject(Map<String, Object> item, String name) {
         var properties = (Map<String, Object>) item.get("properties");
         var required = (ArrayList<String>) item.get("required");
-            var isRequired = (String key) -> required.contains(key);
+        Predicate<String> isRequired = (String key) -> required == null ? false : required.contains(key);
         var params = new ArrayList<String>();
         var javadocs = new ArrayList<String>();
 
@@ -168,8 +224,8 @@ public class GenerateDTOsTask {
                 propName = propName + name;
             }
 
-            params.add(processAndGetType(prop, propName, name, false) + " " + propName);
-            javadocs.add("* @param " + propName + " " + prop.get("description"));
+            params.add(processAndGetType(prop, propName, name, false, isRequired) + " " + propName);
+            javadocs.add("* @param " + propName + " " + (prop.containsKey("description") && !prop.get("description").equals("") ? prop.get("description") : propName));
         }
 
         createJavaClass(item, name, javadocs, params);
@@ -180,6 +236,7 @@ public class GenerateDTOsTask {
                 package io.bokun.octo;
                 
                 import java.util.ArrayList;
+                import javax.annotation.Nonnull;
                 
                 /**
                 * Octo DTO: %s.
@@ -244,7 +301,7 @@ public class GenerateDTOsTask {
         var octoSchemas = (Map<String, Object>) ((Map<String, Object>) octoDefinition.get("components")).get("schemas");
         for (var name : octoSchemas.keySet()) {
             var schema = (Map<String, Object>) octoSchemas.get(name);
-            processAndGetType(schema, name, name, false);
+            processAndGetType(schema, name, name, false, (String n) -> false);
         }
 
         createEnums();
